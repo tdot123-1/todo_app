44f86f5c3200cb1b0e31ae38c250c8107715ef41
Thomas Kruithof <tkruithof95@gmail.com>
2025-01-06 19:50:25 +0100

    setup db

---
+++ server/db.py
@@ -0,0 +1,26 @@
+from typing import Annotated
+from sqlmodel import create_engine, SQLModel, Session
+from fastapi import Depends
+
+# create Task schema
+class Task(SQLModel, table=True):
+    id: int | None
+
+# create db engine
+sqlite_file_name = "todo_app.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+# create tables
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+    
+# create session dependency
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+# 'Annotated' is used to add metadata to type hints
+SessionDep = Annotated[Session, Depends(get_session)]
